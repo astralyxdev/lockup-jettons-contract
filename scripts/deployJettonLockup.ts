dff3bc2720312c2817abcec25e9359a35269b0b3
Kirill Kirilenko <kvkirilenko@alfabank.ru>
2023-02-22 15:51:46 +0400

    add deploy script

--- scripts/deployJettonLockup.ts
+++ scripts/deployJettonLockup.ts
@@ -1,13 +1,24 @@
-import { toNano } from 'ton-core';
+import { Address, beginCell, toNano } from 'ton-core';
 import { JettonLockup } from '../wrappers/JettonLockup';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const jettonLockup = JettonLockup.createFromConfig({}, await compile('JettonLockup'));
-
-    await provider.deploy(jettonLockup, toNano('0.05'));
-
+    const jettonLockup = JettonLockup.createFromConfig(
+        {
+            owner: Address.parse("EQC_UQzCLJniVUDD4AD_uM8_cf22T7-QIHTy3zzfogCtO05A"),
+            receiver: Address.parse("EQC_UQzCLJniVUDD4AD_uM8_cf22T7-QIHTy3zzfogCtO05A"),
+            unlockedAt: (Date.now() / 1000) + 5
+        },
+        await compile('JettonLockup')
+    );
+    await provider.deploy(
+        jettonLockup, 
+        toNano('0.05'), 
+        beginCell()
+            .storeUint(0xd53276db, 32)
+            .storeUint(0, 64)
+            .endCell()
+    );
     const openedContract = provider.open(jettonLockup);
-
-    // run methods on `openedContract`
+    console.log('Address:', openedContract.address)
 }
